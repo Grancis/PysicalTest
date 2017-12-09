7cb81e867677a5621e690697b2af02067b742ae7
Grancis <wzhenchao@live.com>
2017-12-10 01:44:41 +0800

    显示数据的卡片排版做好了，在historyQuery.html界面准备好四个cardBox，css中全部设置为隐藏，每查询到一组数据进行一个cardBox的填充并显示一个cardBox

--- front end/jslib/general.js
+++ front end/jslib/general.js
@@ -1,17 +1,17 @@
 window.onload=function () {
-    var cardBox=document.getElementById("cardBox");
+    var cardBoxes=document.getElementsByClassName("cardBox");
     var card=document.getElementsByClassName("card");
     var header=document.getElementsByClassName("header");
     var msg=document.getElementsByClassName("msg");
     var bean=document.getElementsByClassName("bead");
     var content=document.getElementsByClassName("content");
     var nav=document.getElementById("nav");
-    setWidth(cardBox,640);
-    setWidth(nav,640);
+    for (var i=0;i<cardBoxes.length;++i)
+        setWidth(cardBoxes[i],640);
     for(var i=0;i<card.length;++i)
         setHeight(header[i],card[i]);
 }
 window.onresize=function () {
-    setWidth(cardBox,640);
-    setWidth(nav,640);
+    for (var i=0;i<cardBoxes.length;++i)
+        setWidth(cardBoxes[i],640);
 }
---
+++ front end/jslib/queryData.js
@@ -0,0 +1,9 @@
+var studentData={
+    term:"",
+    name:'',
+    assessment:"",
+    data:[height,weight,lung,jump,shortRun,step,longRunFemale,longRunMale,sitUp,pullUp,sitAndReach,grip,visionLeft,visionRight],
+    score:[height,weight,lung,jump,shortRun,step,longRunFemale,longRunMale,sitUp,pullUp,sitAndReach,grip,visionLeft,visionRight],
+    suggestion:""
+}
+
